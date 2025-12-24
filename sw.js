72a9c8c82f7972df920f8b4f2e25b45e36ae5eea
marokoalex <marokoalex@yahoo.com.tw>
2025-12-24 16:57:29 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+﻿const CACHE_NAME = "pwa-cache-v1";
+const FILES = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
+  );
+});
